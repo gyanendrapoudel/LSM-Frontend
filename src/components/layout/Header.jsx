a2a379f50b0c4296783f4e315c52343aee9757a8
Gyanendra <gyanen56@gmail.com>
2025-02-19 12:05:25 +1100

    changing header component using userdetails

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -9,12 +9,15 @@ import { IoHome } from 'react-icons/io5'
 import { IoIosLogIn } from 'react-icons/io'
 import { FaArrowCircleUp } from 'react-icons/fa'
 import logo from '@/assets/lms-logo.png'
+import { AiFillBank } from 'react-icons/ai'
+import { useSelector } from 'react-redux'
 const  Header = () => {
+  const {user} = useSelector((state)=>state.userInfo)
   return (
     <Navbar expand="lg" className="bg-dark text-light" variant="dark">
       <Container fluid className="mx-5">
         <Link className="nav-link me-5" to="/">
-          <img src={logo} alt="logo" width="100px"/>
+          <img src={logo} alt="logo" width="100px" />
         </Link>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll" className="justify-content-between">
@@ -26,14 +29,25 @@ const  Header = () => {
               Link
             </Link>
           </Nav>
-          <Nav className="d-flex user-container  gap-3">
-            <Link className="nav-link" to="/signin">
-              <IoIosLogIn /> SignIn
-            </Link>
-            <Link className="nav-link" to="/signup">
-              <FaArrowCircleUp /> SignUp
-            </Link>
-          </Nav>
+          {user?._id ? (
+            <Nav className="d-flex user-container  gap-3">
+              <Link className="nav-link" to="/user">
+                <AiFillBank /> Dashboard
+              </Link>
+              <Link className="nav-link" to="/">
+                <FaArrowCircleUp /> Logout
+              </Link>
+            </Nav>
+          ) : (
+            <Nav className="d-flex user-container  gap-3">
+              <Link className="nav-link" to="/signin">
+                <IoIosLogIn /> SignIn
+              </Link>
+              <Link className="nav-link" to="/signup">
+                <FaArrowCircleUp /> SignUp
+              </Link>
+            </Nav>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
